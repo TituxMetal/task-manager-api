02a286ed1ae3d11f1fe842dd01c68e28ce90ef9c
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-29 17:54:04 +0100

    Update config file; add jwtSecret key

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.PORT || 5000,
   mongoOptions: { useCreateIndex: true, useNewUrlParser: true },
-  mongoUri: process.env.MONGODB_URI || 'mongodb://mongo:27017/test'
+  mongoUri: process.env.MONGODB_URI || 'mongodb://mongo:27017/test',
+  jwtSecret: process.env.JWT_SECRET || 'supersecretjsonwebtoken'
 }
