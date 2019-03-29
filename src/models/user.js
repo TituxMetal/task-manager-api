0c47ac81eb0505e7cae6ce91c49ce59d877d8a2b
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-29 16:52:03 +0100

    Update user model; add a method that hash user password before saving in database

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,6 +19,15 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
+  }
+})
+
+userSchema.pre('save', async function() {
+  const user = this
+
+  if (user.isModified('password')) {
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
   }
 })
 
