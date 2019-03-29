fa0e61651d9d83218c2f2ee3004e82067ad3dbdf
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-29 14:29:53 +0100

    Update server.js file; add mongo database and users routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 
+require('./database/mongo')
+
 const server = express()
 
 server.use(express.json())
 server.use('/welcome', require('./routes/welcome'))
+server.use('/users', require('./routes/users'))
 
 module.exports = server
