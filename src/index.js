0edfa0a0f0c9df496398b3a163d6557141b72fc4
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-29 14:31:47 +0100

    Update index.js file; use config file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const server = require('./server')
 
-const port = process.env.PORT || 5000
+const { port } = require('./config')
 
 server.listen(port, () => console.log(`Server is running on port: ${port}`))
