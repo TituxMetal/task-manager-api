599ec5dec04fc524362c37b296f6a5a868df1809
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-31 07:04:55 +0200

    Update server.js file; add routes for /tasks

--- src/server.js
+++ src/server.js
@@ -7,5 +7,6 @@ const server = express()
 server.use(express.json())
 server.use('/welcome', require('./routes/welcome'))
 server.use('/users', require('./routes/users'))
+server.use('/tasks', require('./routes/tasks'))
 
 module.exports = server
