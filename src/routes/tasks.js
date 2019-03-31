3849657ff4a9a8bd5a5d820fceea980a83f6a945
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-31 09:42:33 +0200

    Update tasks routes; add PATCH /tasks/:id

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -20,4 +20,31 @@ router.post('/', auth, async ({ body, user }, res) => {
   }
 })
 
+router.patch('/:id', auth, async ({ body, params, user }, res) => {
+  const updates = Object.keys(body)
+  const allowedUpdates = ['description', 'completed']
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+  const _id = params.id
+  const owner = user._id
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates.' })
+  }
+
+  try {
+    const task = await Task.findOne({ _id, owner })
+
+    if (!task) {
+      return res.status(404).send({ error: 'No task found.' })
+    }
+
+    updates.forEach(update => (task[update] = body[update]))
+    await task.save()
+
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
 module.exports = router
